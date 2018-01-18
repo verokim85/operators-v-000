abca1e738a2dd382accbac24de424cf05a3e7e92
Veronica Kim <vero.a.kim@gmail.com>
2018-01-18 23:32:20 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,12 @@
 def unsafe?(speed)
-	speed > 40 || speed < 60? "unsafe" : "safe"
+	if speed < 40 || speed > 60
+		return true
+	else
+		return false
+end
 end
 
 
-
 def not_safe?(speed)
-
+	speed < 40 || speed > 60? true : false
 end
